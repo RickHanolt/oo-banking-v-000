5990c731dd1f02471ecb81b70e3e9450053df915
Richard Hanolt <rhanolt@gmail.com>
2020-05-24 22:03:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,8 @@ class Transfer
   def execute_transaction
     binding.pry
     if (sender.balance >= @amount) && sender.valid? && @status == "pending"
-      sender.balance = sender.balance - @amount        receiver.balance = receiver.balance + @amount
+      sender.balance = sender.balance - @amount
+      receiver.balance = receiver.balance + @amount
       @status = "complete"
     else
       @status = "rejected"
